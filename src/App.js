70d7df7a6e9c7601ff8e7e57d793ee603dfa5c4b
black-code1 <legrandmahamat1@gmail.com>
2021-08-27 01:22:22 +0100

    Components & Templates

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
 	return (
 		<div className="App">
-			<header className="App-header">
-				<img src={logo} alt="logo" className="App-logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-					className="App-link"
-				>
-					Learn React
-				</a>
-			</header>
+			<div className="content">
+				<h1>App Component</h1>
+			</div>
 		</div>
 	);
 }
